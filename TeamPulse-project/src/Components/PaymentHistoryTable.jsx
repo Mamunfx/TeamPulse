73abf58190bb0733cbb29f0f6bbb962978175b5e
Mamunfx <md01993049420@gmail.com>
2025-01-28 23:27:12 +0600

    100% complete

--- TeamPulse-project/src/Components/PaymentHistoryTable.jsx
+++ TeamPulse-project/src/Components/PaymentHistoryTable.jsx
@@ -8,7 +8,7 @@ const PaymentHistoryTable = ({ payments = [] }) => {
           <thead>
             <tr>
               <th></th>
-              <th>Name</th>
+              <th>Transaction Id</th>
               <th>Salary</th>
               <th>Month</th>
               <th>Year</th>
@@ -18,7 +18,7 @@ const PaymentHistoryTable = ({ payments = [] }) => {
             {payments.map((payment, index) => (
               <tr key={index}>
                 <th>{index + 1}</th>
-                <td>{payment.name}</td>
+                <td>{payment.paymentIntentId}</td>
                 <td>{payment.salary}</td>
                 <td>{payment.month}</td>
                 <td>{payment.year}</td>
